2ddd00b1c5becbb719beb6bef4ad4fced5409b36
Sunil63603 <s60667843@gmail.com>
2026-09-11 11:22:13 +0530

    Integrated Tour, Tooltip, Demo and AppBanner

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import AHDjs from "ahdjs";
 import "ahdjs/build/css/index.css";
 
 const ahdJs = new AHDjs(undefined, {
-  applicationId: "68c413718051925116f90732",
+  applicationId: "6a3d30e35d1dbe1fbd243497",
   apiHost: "https://pagepilot.fabbuilder.com",
   visitorId: "visitor-id",
   showProgressbar: false,
 });
 ahdJs.initializeSiteMap();
-ahdJs.showHighlights("/#skills", true);
+ahdJs.renderAppBanner("SUNIL_BANNER", true);
